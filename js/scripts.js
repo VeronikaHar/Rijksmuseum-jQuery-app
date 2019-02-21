fc61a6e58f76dcb44bed9fb67762b3a22c3c7294
veronikahar <veronika.har@hotmail.com>
2019-02-20 23:10:28 -0600

    Modal that displays art object details upon clicking onto thumbnail added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,96 @@
-      // search the collection using a JSON call
-      function search(query) {
-        return $.getJSON(`https://www.rijksmuseum.nl/api/en/collection?q=${query}&key=XaEeFrSV&format=json`);
-      }
-      
-      let searchBox = $('#query');
-      $("#search-btn").click(() => {
-        // fire the search query
-        search(searchBox.val()).done(function (results) {
-          $('#result-container').empty();
-          var $div = $('#result-container');
-          $div.html('');
-          
-          // create a button for each art object found
-          $.each(results.artObjects, function (index, object) {
-            console.log(object);
-            
-            var $btn = $('<button class="thumbnail">' + '</button>').appendTo($div);
-            $btn.append(thumbnail(object));
-            $btn.append(title(object));
-            
-            // make each row clickable, navigating to the relevant page on the Rijksmuseum website
-            /* $btn.on("click", function() {
-              document.location = object.links.web;
-            }); */
-          })
+  // search the collection using a JSON call
+  function search(query) {
+    return $.getJSON(`https://www.rijksmuseum.nl/api/en/collection?q=${query}&key=XaEeFrSV&format=json`);
+  }
+
+  let searchBox = $('#query');
+  $("#search-btn").click(() => {
+
+    // fire the search query
+    search(searchBox.val()).done(function (results) {
+      $('#result-container').empty();
+      var $div = $('#result-container');
+      $div.html('');
+
+      // create a button for each art object found
+      $.each(results.artObjects, function (index, object) {
+        console.log(object);
+
+        var $btn = $('<button class="thumbnail">' + '</button>').appendTo($div);
+        $btn.append(thumbnail(object));
+        $btn.append(title(object));
+
+        // make each btn clickable, showing the modal with details
+        $btn.on('click', () => {
+          showDetails(object);
+        }); 
+      })
+    });
+  });
+
+  // creates a thumbnail image for the specified art object
+  function thumbnail(object) {
+    if (object.webImage !== null) {
+      return $('<div>').addClass('thumb').css('background-image', 'url(' +
+        object.webImage.url.replace("s0", "s350") + ')');
+    } else {
+      return $('<div>').addClass('thumb').attr('id', 'thumb-w-bg')
+    };
+  }
+
+  // creates title and author info for the specified art object
+  function title(object) {
+    return $('<div class="title">' + '<span>' + object.title + '</span>' + '<br>' +
+      'by ' + '<br>' + object.principalOrFirstMaker + '</div>');
+  }
+
+  // Function that collects art object details
+  function showDetails(object) {
+    (() => {
+      var $modalContainer = $('#modal-container');
+
+      function showModal(title, text) {
+        // Clear all existing modal content
+        $modalContainer.empty();
+
+        var modal = $('<div>').addClass('modal');
+        $modalContainer.append(modal);
+        $modalContainer.addClass('is-visible');
+
+        //Add new modal content
+        modal.append($('<img>').attr('src', object.webImage.url.replace("s0", "s600")))
+        .append($('<h3>').text(title))
+        .append($('<p>').text(text))
+        .append($('<button>').addClass('modal-close').text('Close'));
+
+        $('<button class="modal-close">').on('click', () => {
+          hideModal;
         });
+      }
+
+      function hideModal() {
+        $modalContainer.removeClass('is-visible');
+      }
+
+     // loadDetails(object).then(() => {
+        var objectName=object.longTitle;
+        var objectDetails = 'Object Number: ' + object.objectNumber
+        +'\nFor more details '+ $('<a href="'+object.links.web+'">click here</a>') +'.' ;
+        showModal(objectName, objectDetails);
+      //});
+
+      //Close modal by pressing ESC on the keyboard
+      window.on('keydown', (e) => {
+        if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+          hideModal();
+        }
       });
-      
-      // creates a thumbnail image for the specified art object
-      function thumbnail(object) {
-        if (object.webImage !== null) {
-          return $('<div>').addClass('thumb').css('background-image', 'url(' +
-          object.webImage.url.replace("s0", "s350") + ')');
-        } else {
-          return $('<div>').addClass('thumb').attr('id', 'thumb-w-bg')
-        };
-      }
-      
-      // creates title and author info for the specified art object
-      function title(object) {
-        return $('<div class="title">' + '<span>' + object.title + '</span>' + '<br>' +
-        'by ' + '<br>' + object.principalOrFirstMaker + '</div>');
-      }
+
+      $modalContainer.on('click', (e) => {
+        var target = e.target;
+        if (target === $modalContainer) {
+          hideModal();
+        }
+      });
+    })();
+  }
