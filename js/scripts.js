9610d9cdff70185acdebe4d1943a9c07ca83fb37
veronikahar <veronika.har@hotmail.com>
2019-02-26 14:43:49 -0600

    ESLint errors fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
-var artResults = (() => {
+/* eslint-disable no-undef */
+(() => {
   // search the collection using a JSON call
   function search(query) {
     return $.getJSON(`https://www.rijksmuseum.nl/api/en/collection?q=${query}&key=XaEeFrSV&format=json`);
   }
 
-  $(function() {
+  $(function () {
     fireSearch('artists');
   });
 
   function fireSearch(entry) {
-    
+
     // fire the search query
     search(entry).done(function (results) {
       $('#result-container').empty();
@@ -35,7 +36,7 @@ var artResults = (() => {
   }
 
   let searchBox = $('#query');
-  
+
   //Search by clicking Search btn
   $('#search-btn').click(() => {
     fireSearch(searchBox.val());
@@ -43,7 +44,7 @@ var artResults = (() => {
 
   //Search by pressing Enter on keyboard
   $('#query').on('keydown', (e) => {
-    if (e.key === 'Enter'){
+    if (e.key === 'Enter') {
       fireSearch(searchBox.val());
     }
   });
@@ -52,16 +53,16 @@ var artResults = (() => {
   function thumbnail(object) {
     if (object.webImage !== null) {
       return $('<div>').addClass('thumb').css('background-image', 'url(' +
-        object.webImage.url.replace("s0", "s350") + ')');
+        object.webImage.url.replace('s0', 's350') + ')');
     } else {
       return $('<div>').addClass('thumb').attr('id', 'thumb-w-bg')
-    };
+    }
   }
 
   // creates title and author info for the specified art object
   function title(object) {
     return $('<div class="title">' + '<h3>' + object.title + '</h3>' + '<p>' +
-      'by '+'<br>'  + object.principalOrFirstMaker+ '</p>' + '</div>');
+      'by ' + '<br>' + object.principalOrFirstMaker + '</p>' + '</div>');
   }
 })();
 
@@ -71,31 +72,40 @@ function showDetails(object) {
     var $modalContainer = $('#modal-container');
     var imgEl = $('<img>');
     if (object.webImage !== null) {
-      imgEl.attr('src', object.webImage.url.replace("s0", "s600"));
+      imgEl.attr('src', object.webImage.url.replace('s0', 's600'));
     } else {
       imgEl.attr('id', 'thumb-w-bg');
-    };
+    }
 
     function loadDetails(object) {
-      var url='https://cors.io/?'+object.links.self+'?key=XaEeFrSV&format=json'
-      return $.ajax(url, { dataType: 'json' }).then( (details) => {
-        
+      var url = 'https://cors.io/?' + object.links.self + '?key=XaEeFrSV&format=json'
+      return $.ajax(url, {
+        dataType: 'json'
+      }).then((details) => {
+
         // Now we add the details to the art object item
-        if(details.artObject.label.description){
-          object.artDescr=details.artObject.label.description;
-        } else {object.artDescr='Description of the art piece is not available.'};
-        if(details.artObject.physicalMedium){
-          object.artMedium=details.artObject.physicalMedium;
-        } else {object.artMedium='unknown art medium'};
-        if(details.artObject.subTitle){
-          object.artDims=details.artObject.subTitle;
-        } else {object.artDims='dimensions not available'}
-      }).catch( (e) => {
-          console.error(e);          
+        if (details.artObject.label.description) {
+          object.artDescr = details.artObject.label.description;
+        } else {
+          object.artDescr = 'Description of the art piece is not available.'
+        }
+        if (details.artObject.physicalMedium) {
+          object.artMedium = details.artObject.physicalMedium;
+        } else {
+          object.artMedium = 'unknown art medium'
+        }
+        if (details.artObject.subTitle) {
+          object.artDims = details.artObject.subTitle;
+        } else {
+          object.artDims = 'dimensions not available'
+        }
+      }).catch((e) => {
+        // eslint-disable-next-line no-console
+        console.error(e);
       });
     }
 
-    loadDetails(object).then( () => {
+    loadDetails(object).then(() => {
       showModal()
     });
 
@@ -111,14 +121,14 @@ function showDetails(object) {
 
       //Add new modal content
       modal.append(imgEl)
-        .append($('<h3>').html(object.longTitle+'<p>'+object.artMedium+', '+object.artDims+'</p>'))
-        .append($('<p>').html('<span>'+object.artDescr +'</span>'+ '<br>'+ '<br>'+'Art Object Number: ' + object.objectNumber + '<br>' + 'For more details go to ').append(hyperlink))
+        .append($('<h3>').html(object.longTitle + '<p>' + object.artMedium + ', ' + object.artDims + '</p>'))
+        .append($('<p>').html('<span>' + object.artDescr + '</span>' + '<br>' + '<br>' + 'Art Object Number: ' + object.objectNumber + '<br>' + 'For more details go to ').append(hyperlink))
         .append($('<button>').addClass('modal-close').text('Close').click(hideModal));
-    };
+    }
 
     function hideModal() {
       $modalContainer.removeClass('is-visible');
-    };
+    }
 
     showModal();
 
